bd35f7f4f17c43df6ed2d26f20a1caf54d9f4c8f
Nishank <nishankpr2002@gmail.com>
2021-12-11 02:01:48 +0530

    error_handles_added

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 require('dotenv').config({path: "./config.env"})
 const express = require('express')
 const authRoute = require('./routes/auth')
+const errorHandler = require('./middleware/error')
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.use('/auth', authRoute)
+
+// Errorhandler
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3000
 
