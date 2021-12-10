c3e8fcf9a201853c0cbe04267b190445caa50515
Nishank <nishankpr2002@gmail.com>
2021-12-10 12:06:57 +0530

    search_updated

--- client/src/components/channelsearch/ChannelSearch.js
+++ client/src/components/channelsearch/ChannelSearch.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import { useChatContext } from 'stream-chat-react'
+import SearchIcon from '@material-ui/icons/Search';
 
 const ChannelSearch = () => {
     return (
