f088da928ecfa06f567760add37364ef2bc61486
Nishank <nishankpr2002@gmail.com>
2021-12-10 22:49:05 +0530

    client_auth_added

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,18 @@ import { StreamChat } from 'stream-chat'
 import { Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 
-import { Channels, Channel } from './components'
+import { Channels, Channel, Auth } from './components'
 
 const ApiKey = '4hjn887t4ape'
+
+const authToken = false
 
 const client = StreamChat.getInstance(ApiKey)
 
 function App() {
+
+  if (!authToken) return <Auth/>
+
   return (
     <AppContainer>
       <Chat client={client} theme="team light">
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Channels } from './channels/Channels'
 export { default as Channel } from './channel/Channel'
+export { default as Auth } from './auth/Auth'
