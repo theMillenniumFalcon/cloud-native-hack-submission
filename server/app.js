9e88c5f57345989dab0e1295303544d31c06a35d
Nishank <nishankpr2002@gmail.com>
2021-12-12 08:00:25 +0530

    twilio_added

--- server/app.js
+++ server/app.js
@@ -9,10 +9,38 @@ const corsOptions = {
     optionSuccessStatus:200,
 }
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID
+const twilioClient = require('twilio')(accountSid, authToken)
+
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors(corsOptions))
+
+app.post('/', (req, res) => {
+    const { message, user: sender, type, members } = req.body;
+
+    if(type === 'message.new') {
+        members
+            .filter((member) => member.user_id !== sender.id)
+            .forEach(({ user }) => {
+                if(!user.online) {
+                    twilioClient.messages.create({
+                        body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+                        messagingServiceSid: messagingServiceSid,
+                        to: user.phoneNumber
+                    })
+                        .then(() => console.log('Message sent!'))
+                        .catch((err) => console.log(err))
+                }
+            })
+
+            return res.status(200).send('Message sent!')
+    }
+    return res.status(200).send('Not a new message request')
+})
 
 app.use('/auth', authRoute)
 
