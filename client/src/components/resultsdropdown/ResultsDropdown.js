c259e3ddaeaf088af4e0c184890670a113999684
Nishank <nishankpr2002@gmail.com>
2021-12-12 07:08:15 +0530

    result_jsx_updated

--- client/src/components/resultsdropdown/ResultsDropdown.js
+++ client/src/components/resultsdropdown/ResultsDropdown.js
@@ -25,53 +25,85 @@ const SearchResult = ({ channel, focusedId, type, setChannel, setToggleContainer
 
   if (type === 'channel') {
     return (
-      <div
-        onClick={() => {
-          setChannel(channel)
-          if(setToggleContainer) {
-            setToggleContainer((prevState) => !prevState)   
-          }
-        }}
-        className={focusedId === channel.id ? 'channel-search__result-container__focused' : 'channel-search__result-container' }
-      >
-        <div className='result-hashtag'>#</div>
-        <p className='channel-search__result-text'>{channel.data.name}</p>
-      </div>
+        <>
+            {(focusedId === channel.id) ? (
+            <ChannelResultFocused onClick={() => {
+                setChannel(channel)
+                if(setToggleContainer) {
+                  setToggleContainer((prevState) => !prevState)   
+                }
+              }}>
+                  <ResultHashtag></ResultHashtag>
+                  <ChannelResultText>
+                      <p>{channel.data.name}</p>
+                  </ChannelResultText>
+            </ChannelResultFocused>
+            ) : (
+            <ChannelResult onClick={() => {
+                setChannel(channel)
+                if(setToggleContainer) {
+                  setToggleContainer((prevState) => !prevState)   
+                }
+              }}>
+                    <ResultHashtag></ResultHashtag>
+                    <ChannelResultText>
+                        <p>{channel.data.name}</p>
+                    </ChannelResultText>
+            </ChannelResult>
+        )}
+        </>
     );
   }
 
   return (
-    <div
-      onClick={async () => {
-        channelByUser({ client, setActiveChannel, channel, setChannel })
-        if(setToggleContainer) {
-            setToggleContainer((prevState) => !prevState)   
-        }
-      }}
-      className={focusedId === channel.id ? 'channel-search__result-container__focused' : 'channel-search__result-container' }
-    >
-      <div className='channel-search__result-user'>
-        <Avatar image={channel.image || undefined} name={channel.name} size={24} />
-        <p className='channel-search__result-text'>{channel.name}</p>
-      </div>
-    </div>
+    <>
+        {(focusedId === channel.id) ? (
+            <ChannelResultFocused onClick={async () => {
+                channelByUser({ client, setActiveChannel, channel, setChannel })
+                if(setToggleContainer) {
+                    setToggleContainer((prevState) => !prevState)   
+                }
+              }}>
+                    <ChannelResultUser>
+                        <Avatar image={channel.image || undefined} name={channel.name} size={24} />
+                        <ChannelResultText>
+                            <p>{channel.data.name}</p>
+                        </ChannelResultText>
+                    </ChannelResultUser>
+            </ChannelResultFocused>
+            ) : (
+            <ChannelResult onClick={async () => {
+                channelByUser({ client, setActiveChannel, channel, setChannel })
+                if(setToggleContainer) {
+                    setToggleContainer((prevState) => !prevState)   
+                }
+              }}>
+                    <ChannelResultUser>
+                        <Avatar image={channel.image || undefined} name={channel.name} size={24} />
+                        <ChannelResultText>
+                            <p>{channel.data.name}</p>
+                        </ChannelResultText>
+                    </ChannelResultUser>
+            </ChannelResult>
+        )}
+    </>
   );
 };
 
 const ResultsDropdown = ({ teamChannels, directChannels, focusedId, loading, setChannel, setToggleContainer }) => {
 
   return (
-    <div className='channel-search__results'>
-      <p className='channel-search__results-header'>Channels</p>
+    <ChannelSearchResults>
+      <ChannelSearchResultsHeader>Channels</ChannelSearchResultsHeader>
       {loading && !teamChannels.length && (
-        <p className='channel-search__results-header'>
+        <ChannelSearchResultsHeader>
           <i>Loading...</i>
-        </p>
+        </ChannelSearchResultsHeader>
       )}
       {!loading && !teamChannels.length ? (
-        <p className='channel-search__results-header'>
+        <ChannelSearchResultsHeader>
           <i>No channels found</i>
-        </p>
+        </ChannelSearchResultsHeader>
       ) : (
         teamChannels?.map((channel, i) => (
           <SearchResult
@@ -84,16 +116,16 @@ const ResultsDropdown = ({ teamChannels, directChannels, focusedId, loading, set
           />
         ))
       )}
-      <p className='channel-search__results-header'>Users</p>
+      <ChannelSearchResultsHeader>Users</ChannelSearchResultsHeader>
       {loading && !directChannels.length && (
-        <p className='channel-search__results-header'>
+        <ChannelSearchResultsHeader>
           <i>Loading...</i>
-        </p>
+        </ChannelSearchResultsHeader>
       )}
       {!loading && !directChannels.length ? (
-        <p className='channel-search__res ults-header'>
+        <ChannelSearchResultsHeader>
           <i>No direct messages found</i>
-        </p>
+        </ChannelSearchResultsHeader>
       ) : (
         directChannels?.map((channel, i) => (
           <SearchResult
@@ -106,8 +138,89 @@ const ResultsDropdown = ({ teamChannels, directChannels, focusedId, loading, set
           />
         ))
       )}
-    </div>
+    </ChannelSearchResults>
   )
 }
+
+const ChannelResultFocused = styled.div`
+width: 100%;
+height: 48px;
+display: flex;
+align-items: center;
+background: #005fff1a;
+`;
+
+const ChannelResult = styled.div`
+width: 100%;
+height: 48px;
+display: flex;
+align-items: center;
+&:hover {
+    background: #005fff1a;
+    cursor: pointer;
+}
+`;
+
+const ResultHashtag = styled.div`
+height: 24px;
+width: 28px;
+background: #005fff;
+border-radius: 24px;
+margin: 12px;
+display: flex;
+align-items: center;
+justify-content: center;
+font-weight: bold;
+font-size: 14px;
+line-height: 120%;
+color: #ffffff;
+`;
+
+const ChannelResultText = styled.div`
+p {
+    width: 100%;
+    font-family: Helvetica Neue, sans-serif;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 120%;
+    color: #2c2c30;
+}
+`;
+
+const ChannelResultUser = styled.div`
+display: flex;
+align-items: center;
+margin-left: 12px;
+`;
+
+const ChannelSearchResults = styled.div`
+position: absolute;
+height: fit-content;
+width: 300px;
+background: #fff;
+border: 1px solid #e9e9ea;
+box-sizing: border-box;
+box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.06);
+border-radius: 8px;
+z-index: 10;
+left: 230px;
+top: 16px;
+`;
+
+const ChannelSearchResultsHeader = styled.div`
+width: fit-content;
+display: flex;
+align-items: center;
+font-style: normal;
+font-weight: 500;
+font-size: 14px;
+line-height: 120%;
+color: #858688;
+margin-left: 12px;
+i {
+    font-weight: normal;
+    margin-left: 12px;
+}
+`;
 
 export default ResultsDropdown
